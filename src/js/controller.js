306fc38f377925c5d9b5937772734efd50dd501b
Sahilm-98 <sahilm8540@gmail.com>
2024-08-08 08:59:56 -0700

    Implementing Search Results - Part 2

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,17 +1,16 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
-
+import resultsView from './views/resultsView.js';
 import 'core-js/stable';
 
 import 'regenerator-runtime/runtime';
 import recipeView from './views/recipeView.js';
+import { async } from 'regenerator-runtime';
 
-const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipe = async function () {
   try {
@@ -33,15 +32,17 @@ const controlRecipe = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
     // Get search query
     const query = searchView.getQuery();
-    if (!queryuery) return;
+    if (!query) return;
 
     // Load search Results
     await model.loadSearchResults('query');
 
     // Render Search Results
     console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,16 +1,16 @@
 class searchView {
-  #parentEl = document.querySelector('.search');
+  _parentElement = document.querySelector('.search');
 
   getQuery() {
-    const query = this.#parentEl.querySelector('.search__field').value;
-    this.#clearInput();
+    const query = this._parentElement.querySelector('.search__field').value;
+    this._clearInput();
     return query;
   }
-  #clearInput() {
-    this.#parentEl.querySelector('.search__field').value = '';
+  _clearInput() {
+    this._parentElement.querySelector('.search__field').value = '';
   }
   addHandlerSearch(handler) {
-    this.#parentEl.querySelector('submit', function (e) {
+    this._parentElement.querySelector('submit', function (e) {
       e.preventDefault();
       handler();
     });
